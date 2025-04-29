93b6911806281bd03726bccdcc5c05877173f003
lwvnct <lowievincent.canonigo@carsu.edu.ph>
2025-04-29 09:11:03 +0800

    updated

--- types/generated/contentTypes.d.ts
+++ types/generated/contentTypes.d.ts
@@ -385,6 +385,7 @@ export interface ApiHeaderPerProjectSectionHeaderPerProjectSection
     createdAt: Schema.Attribute.DateTime;
     createdBy: Schema.Attribute.Relation<'oneToOne', 'admin::user'> &
       Schema.Attribute.Private;
+    delivery_date: Schema.Attribute.String;
     items: Schema.Attribute.Relation<
       'oneToMany',
       'api::project-item.project-item'
@@ -737,6 +738,7 @@ export interface ApiProjectProject extends Struct.CollectionTypeSchema {
     createdAt: Schema.Attribute.DateTime;
     createdBy: Schema.Attribute.Relation<'oneToOne', 'admin::user'> &
       Schema.Attribute.Private;
+    delivery_date: Schema.Attribute.String;
     dueDate: Schema.Attribute.Date;
     header_per_project_sections: Schema.Attribute.Relation<
       'oneToMany',
